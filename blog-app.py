c027895d8d5128943caf93e9e5b36d83143674e8
Anthony Givans <58392771+GivyBoy@users.noreply.github.com>
2021-04-02 19:16:51 -0500

    Update blog-app.py

--- blog-app.py
+++ blog-app.py
@@ -28,8 +28,11 @@ if st.button('Submit'):
 
     
     plt.figure(figsize=(16,8))    
+    plt.title(f"{ticker}'s Stock Price")
     plt.plot(stock_price, linewidth= 0.8, color='green')
     plt.fill_between(stock_price.index, stock_price, color='g', alpha=0.4)
+    plt.xlabel('Date')
+    plt.ylabel('Stock Price ($JMD)')
     plt.ylim(0)
     plt.grid(True)
     st.pyplot()
@@ -46,15 +49,20 @@ if st.button('Submit'):
         else:
             col.append('red')
 
-    plt.figure(figsize=(12,6))        
+    plt.figure(figsize=(12,6))       
+    plt.title('Volatility of the stock price')
     plt.bar(returns.index, returns.values, color = col, width=15)
+    plt.xlabel('Date')
+    plt.ylabel('Change in Price')
     st.pyplot()
     with st.beta_expander("See explanation"):
         st.write("""This chart shows the daily fluctuations of the stock's price. If you see a lot of green and not much red, the stock has been performing really well :) """)
     st.write('')
     
     data_processed = [i for i in returns if i!=0]
+    plt.title('Distribution of Volatility')
     sns.displot(data=data_processed, kde=True, bins=80, color='green', alpha=0.4, height=6, aspect=2)
+    plt.xlabel('Change in Price')
     st.pyplot()
     with st.beta_expander("See explanation"):
         st.write("""This chart shows the distribution of the fluctuations of the stock price. If most of the negative fluctuations are concentrated between 0 and -0.1, it's a good thing. It means that, statistically, when the stock price is drecreasing, it won't decrease by much. Also, note that I removed all non-changes (zeroes). """)
@@ -63,8 +71,10 @@ if st.button('Submit'):
     
     drawdown = j.drawdown(returns)
     plt.figure(figsize=(12,6))
-    plt.title('Drawdown')
+    plt.title('Drawdown (Loss of value)')
     drawdown['Drawdown'].plot(color='red', linewidth= 0.8,).autoscale(axis='x', tight=True)
+    plt.xlabel('Date')
+    plt.ylabel('Change in Price')
     plt.fill_between(drawdown.index, drawdown['Drawdown'],color='r', alpha=0.4)
     plt.grid(True)
     st.pyplot()
@@ -73,8 +83,11 @@ if st.button('Submit'):
     st.write('')
 
     wealth_index = 1000*(1+returns).cumprod()
+    plt.title('Cumulative Returns ($JMD)')
     plt.plot(wealth_index, linewidth= 0.8,  color='green')
     plt.fill_between(wealth_index.index, wealth_index, color='g',alpha=0.4)
+    plt.xlabel('Date')
+    plt.ylabel('Portfolio Value ($JMD)')
     plt.ylim(0)
     plt.grid(True)
     st.pyplot()
